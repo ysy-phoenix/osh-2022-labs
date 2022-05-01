78e2c88a039cb440ec2262bf234d3cb06937165f
ysy-phoenix <ysy-phoenix@users.noreply.github.com>
2022-05-01 17:00:38 +0800

    update lab2 for redirect

--- lab2/shell/shell.cpp
+++ lab2/shell/shell.cpp
@@ -11,7 +11,11 @@
 // POSIX API
 #include <unistd.h>
 // wait
+#include <algorithm>
+#include <fcntl.h>
 #include <sys/wait.h>
+// file
+#include <fstream>
 
 #include "shell.h"
 
@@ -40,7 +44,7 @@ int main() {
 
         // 读入一行。std::getline 结果不包含换行符。
         std::getline(std::cin, cmd);
-        cmd = Trim(cmd);
+        cmd = trim(cmd);
 
         // 按管道符拆解命令行
         std::vector<std::string> cmds = split(cmd, "|");
@@ -51,9 +55,31 @@ int main() {
             // 按空格分割命令为单词
             std::vector<std::string> args = split(cmds[0], " ");
 
-            execute(args);
+            // 内置命令
+            if (exeBuiltinCmd(args) == 0) {
+                continue;
+            }
+
+            // 外部命令
+            pid_t pid = fork();
+
+            if (pid == 0) {
+                // 这里只有子进程才会进入
+                // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
+                // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
+                exeSingleCmd(args);
+
+                // 所以这里直接报错
+                exit(255);
+            }
+
+            // 这里只有父进程（原进程）才会进入
+            int ret = wait(nullptr);
+            if (ret < 0) {
+                std::cout << "wait failed\n";
+            }
         } else { // 多管道命令
-            exec_pipe(cmds);
+            exePipeCmd(cmds);
         }
     }
 }
--- lab2/shell/shell.h
+++ lab2/shell/shell.h
@@ -4,33 +4,34 @@
 #define WRITE_END 1 // pipe write end
 #define READ_END 0  // pipe read end
 
-inline std::string &LeftTrim(std::string &s, const char *t = " \t\n\r\f\v");
-inline std::string &RightTrim(std::string &s, const char *t = " \t\n\r\f\v");
-inline std::string &Trim(std::string &s, const char *t = " \t\n\r\f\v");
+inline std::string &leftTrim(std::string &s, const char *t = " \t\n\r\f\v");
+inline std::string &rightTrim(std::string &s, const char *t = " \t\n\r\f\v");
+inline std::string &trim(std::string &s, const char *t = " \t\n\r\f\v");
 std::vector<std::string> split(std::string s, const std::string &delimiter);
-int exec_cd(std::vector<std::string> args);
-int exec_pwd(std::vector<std::string> args);
-int exec_export(std::vector<std::string> args);
-int exec_exit(std::vector<std::string> args);
-int exec_builtin(std::vector<std::string> args);
-void execute(std::vector<std::string> args);
-void exec_pipe(std::vector<std::string> args);
+int exeCd(std::vector<std::string> args);
+int exePwd(std::vector<std::string> args);
+int exeExport(std::vector<std::string> args);
+int exeExit(std::vector<std::string> args);
+int exeBuiltinCmd(std::vector<std::string> args);
+void exeSingleCmd(std::vector<std::string> args);
+void exePipeCmd(std::vector<std::string> args);
+void redirect(std::vector<std::string> &args);
 
 // trim from left
-inline std::string &LeftTrim(std::string &s, const char *t) {
+inline std::string &leftTrim(std::string &s, const char *t) {
     s.erase(0, s.find_first_not_of(t));
     return s;
 }
 
 // trim from right
-inline std::string &RightTrim(std::string &s, const char *t) {
+inline std::string &rightTrim(std::string &s, const char *t) {
     s.erase(s.find_last_not_of(t) + 1);
     return s;
 }
 
 // trim from left & right
-inline std::string &Trim(std::string &s, const char *t) {
-    return LeftTrim(RightTrim(s, t), t);
+inline std::string &trim(std::string &s, const char *t) {
+    return leftTrim(rightTrim(s, t), t);
 }
 
 // 经典的 cpp string split 实现
@@ -39,18 +40,18 @@ std::vector<std::string> split(std::string s, const std::string &delimiter) {
     std::vector<std::string> res;
     size_t pos = 0;
     std::string token;
-    s = Trim(s);
+    s = trim(s);
     while ((pos = s.find(delimiter)) != std::string::npos) {
         token = s.substr(0, pos);
         res.push_back(token);
         s = s.substr(pos + delimiter.length());
-        s = Trim(s);
+        s = trim(s);
     }
     res.push_back(s);
     return res;
 }
 
-int exec_cd(std::vector<std::string> args) {
+int exeCd(std::vector<std::string> args) {
     if (args.size() <= 1) {
         // 输出的信息尽量为英文，非英文输出（其实是非 ASCII 输出）在没有特别配置的情况下（特别是 Windows
         // 下）会乱码 如感兴趣可以自行搜索 GBK Unicode UTF-8 Codepage UTF-16 等进行学习
@@ -67,7 +68,7 @@ int exec_cd(std::vector<std::string> args) {
     return 0;
 }
 
-int exec_pwd(std::vector<std::string> args) {
+int exePwd(std::vector<std::string> args) {
     std::string cwd;
 
     // 预先分配好空间
@@ -84,7 +85,7 @@ int exec_pwd(std::vector<std::string> args) {
     return 0;
 }
 
-int exec_export(std::vector<std::string> args) {
+int exeExport(std::vector<std::string> args) {
     for (auto i = ++args.begin(); i != args.end(); i++) {
         std::string key = *i;
 
@@ -107,7 +108,7 @@ int exec_export(std::vector<std::string> args) {
     return 0;
 }
 
-int exec_exit(std::vector<std::string> args) {
+int exeExit(std::vector<std::string> args) {
     if (args.size() <= 1) {
         exit(0);
     }
@@ -127,22 +128,22 @@ int exec_exit(std::vector<std::string> args) {
     return 0;
 }
 
-int exec_builtin(std::vector<std::string> args) {
+int exeBuiltinCmd(std::vector<std::string> args) {
     // 没有可处理的命令
     if (args.empty()) {
         return 0;
     } else if (args[0] == "cd") {
         // 更改工作目录为目标目录
-        exec_cd(args);
+        exeCd(args);
     } else if (args[0] == "pwd") {
         // 显示当前工作目录
-        exec_pwd(args);
+        exePwd(args);
     } else if (args[0] == "export") {
         // 设置环境变量
-        exec_export(args);
+        exeExport(args);
     } else if (args[0] == "exit") {
         // 退出
-        exec_exit(args);
+        exeExit(args);
     } else {
         // 不是内置指令
         return -1;
@@ -150,14 +151,13 @@ int exec_builtin(std::vector<std::string> args) {
     return 0;
 }
 
-void execute(std::vector<std::string> args) {
+void exeSingleCmd(std::vector<std::string> args) {
     // 运行内置命令
-    if (exec_builtin(args) == 0) {
-        return;
+    if (exeBuiltinCmd(args) == 0) {
+        exit(0);
     }
 
-    // 外部命令
-    pid_t pid = fork();
+    redirect(args);
 
     // std::vector<std::string> 转 char **
     char *arg_ptrs[args.size() + 1];
@@ -167,28 +167,17 @@ void execute(std::vector<std::string> args) {
     // exec p 系列的 argv 需要以 nullptr 结尾
     arg_ptrs[args.size()] = nullptr;
 
-    if (pid == 0) {
-        // 这里只有子进程才会进入
-        // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
-        // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
-        execvp(args[0].c_str(), arg_ptrs);
+    execvp(args[0].c_str(), arg_ptrs);
 
-        // 所以这里直接报错
-        exit(255);
-    }
-
-    // 这里只有父进程（原进程）才会进入
-    int ret = wait(nullptr);
-    if (ret < 0) {
-        std::cout << "wait failed";
-    }
+    return;
 }
 
-void exec_pipe(std::vector<std::string> cmds) {
-    int read_fd; // 上一个管道的读端口（出口）
+void exePipeCmd(std::vector<std::string> cmds) {
+    int lastReadEnd; // 上一个管道的读端口（出口）
     int cnt = cmds.size();
+
     for (int i = 0; i < cnt; i++) {
-        int pipefd[2], ret, lastReadEnd;
+        int pipefd[2], ret;
         // 创建管道
         if (i < cnt - 1) {
             ret = pipe(pipefd);
@@ -202,11 +191,6 @@ void exec_pipe(std::vector<std::string> cmds) {
         pid_t pid = fork();
 
         std::vector<std::string> args = split(cmds[i], " ");
-        char *arg_ptrs[args.size() + 1];
-        for (auto i = 0; i < args.size(); i++) {
-            arg_ptrs[i] = &args[i][0];
-        }
-        arg_ptrs[args.size()] = nullptr;
 
         if (pid == 0) {
             // 除最后一条命令，都将标准输出重定向到当前管道写口
@@ -218,10 +202,7 @@ void exec_pipe(std::vector<std::string> cmds) {
                 dup2(lastReadEnd, STDIN_FILENO);
             }
 
-            if (exec_builtin(args) == 0) {
-                continue;
-            }
-            execvp(args[0].c_str(), arg_ptrs);
+            exeSingleCmd(args);
             exit(255);
         }
         if (i > 0) {
@@ -237,4 +218,47 @@ void exec_pipe(std::vector<std::string> cmds) {
     while (wait(nullptr) > 0)
         ;
 }
+
+void redirect(std::vector<std::string> &args) {
+    /* 支持基本的文件重定向 */
+    std::vector<std::string>::iterator it;
+    if ((it = find(args.begin(), args.end(), ">")) != args.end()) {
+        std::string file = *(it + 1);
+        int fd = open(&file[0], O_RDWR);
+        if (fd == -1) {
+            std::cout << file << ": No such file or directory\n";
+            exit(2);
+        }
+        dup2(fd, STDOUT_FILENO); //将标准输出重定向
+        close(fd);
+        *it = "";
+        *(it + 1) = "";
+    }
+    if ((it = find(args.begin(), args.end(), ">>")) != args.end()) {
+        std::string file = *(it + 1);
+        int fd = open(&file[0], O_RDWR | O_APPEND);
+        if (fd == -1) {
+            std::cout << file << ": No such file or directory\n";
+            exit(2);
+        }
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+        *it = "";
+        *(it + 1) = "";
+    }
+    if ((it = find(args.begin(), args.end(), "<")) != args.end()) {
+        std::string file = *(it + 1);
+        int fd = open(&file[0], O_RDWR);
+        if (fd == -1) {
+            std::cout << file << ": No such file or directory\n";
+            exit(2);
+        }
+        dup2(fd, STDIN_FILENO); // 将标准输入重定向
+        close(fd);
+        *it = "";
+        *(it + 1) = "";
+    }
+    args.erase(remove(args.begin(), args.end(), ""), args.end());
+} // end orient if
+
 #endif
